bb3af7df7c89ff76ef54bd6f1f7b0fbe252aadfd
kxcvbnm <khunanont.ti@gmail.com>
2025-10-04 20:30:00 +0700

    update UserController

--- backend/src/main/java/com/bookingsystem/booking/controllers/UserController.java
+++ backend/src/main/java/com/bookingsystem/booking/controllers/UserController.java
@@ -1,5 +1,6 @@
 package com.bookingsystem.booking.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
@@ -10,11 +11,17 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.bookingsystem.booking.dto.UserDTO;
+import com.bookingsystem.booking.dto.requestdto.user.UserCreateRequest;
+import com.bookingsystem.booking.dto.requestdto.user.UserUpdateRequest;
+import com.bookingsystem.booking.dto.returndto.UserDTO;
 import com.bookingsystem.booking.mappers.UserMapper;
 import com.bookingsystem.booking.models.User;
 import com.bookingsystem.booking.services.UserService;
+
+import jakarta.validation.Valid;
+
 
 @RestController
 @RequestMapping("/api/users")
@@ -27,20 +34,31 @@ public class UserController {
     }
     
     @PostMapping
-    public ResponseEntity<UserDTO> createUser(@RequestBody User user) {
-        User createdUser = userService.createUser(user);
-        return ResponseEntity.status(201).body(UserMapper.toDto(createdUser)); // 201 Created
+    public ResponseEntity<UserDTO> createUser(@Valid @RequestBody UserCreateRequest req) {
+        User createdUser = userService.createUser(req);
+        UserDTO body = UserMapper.toDto(createdUser);
+        URI location = ServletUriComponentsBuilder
+                       .fromCurrentRequest().path("/{id}")
+                       .buildAndExpand(createdUser.getId()).toUri();
+        return ResponseEntity.created(location).body(body); // 201 Created
     }
 
     @GetMapping
     public ResponseEntity<List<UserDTO>> getAllUser() {
-        return ResponseEntity.ok(UserMapper.toDtoList(userService.getAllUser()));
+        List<User> users = userService.getAllUser();
+        return ResponseEntity.ok(UserMapper.toDtoList(users));
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<UserDTO> getUserById(@PathVariable Long id) {
         User user = userService.getUserById(id);
         return ResponseEntity.ok(UserMapper.toDto(user));
+    }
+
+    public ResponseEntity<UserDTO> updateUser(@PathVariable Long id, 
+                                              @Valid @RequestBody UserUpdateRequest req) {
+        User updated = userService.updateUser(id, req);
+        return ResponseEntity.ok(UserMapper.toDto(updated));
     }
 
     @DeleteMapping("/{id}")
