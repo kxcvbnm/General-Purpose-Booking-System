b07cae1eeec7fadaf2b9a1415af668d34ab6b7b5
kxcvbnm <khunanont.ti@gmail.com>
2025-10-04 21:25:12 +0700

    add PatchMapping

--- backend/src/main/java/com/bookingsystem/booking/controllers/UserController.java
+++ backend/src/main/java/com/bookingsystem/booking/controllers/UserController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -55,6 +56,7 @@ public class UserController {
         return ResponseEntity.ok(UserMapper.toDto(user));
     }
 
+    @PatchMapping("/{id}")
     public ResponseEntity<UserDTO> updateUser(@PathVariable Long id, 
                                               @Valid @RequestBody UserUpdateRequest req) {
         User updated = userService.updateUser(id, req);
