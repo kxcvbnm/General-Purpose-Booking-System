9a44c7d02607a1b2a0e2c2e91e0bbbbb80ff9d0f
kxcvbnm <khunanont.ti@gmail.com>
2025-10-10 00:10:57 +0700

    add findByEmailIgnoreCase

--- backend/src/main/java/com/bookingsystem/booking/user/data/UserRepository.java
+++ backend/src/main/java/com/bookingsystem/booking/user/data/UserRepository.java
@@ -1,10 +1,14 @@
 package com.bookingsystem.booking.user.data;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.bookingsystem.booking.user.domain.entities.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    
     boolean existsByEmailIgnoreCase(String email);
     boolean existsByEmailIgnoreCaseAndIdNot(String email, Long id);
+    Optional<User> findByEmailIgnoreCase(String email);
 }
