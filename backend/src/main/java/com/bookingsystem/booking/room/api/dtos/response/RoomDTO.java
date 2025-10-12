9f399563e0894f8ae5602e3c93be35beba6b4467
kxcvbnm <khunanont.ti@gmail.com>
2025-10-12 23:06:12 +0700

    change response dto to record

--- backend/src/main/java/com/bookingsystem/booking/room/api/dtos/response/RoomDTO.java
+++ backend/src/main/java/com/bookingsystem/booking/room/api/dtos/response/RoomDTO.java
@@ -2,27 +2,8 @@ package com.bookingsystem.booking.room.api.dtos.response;
 
 import com.bookingsystem.booking.room.domain.enums.RoomType;
 
-public class RoomDTO {
-   
-    private final Long id;
-    private final String name;
-    private final RoomType roomType;
-
-    public RoomDTO(Long id, String name, RoomType roomType) {
-        this.id = id;
-        this.name = name;
-        this.roomType = roomType;
-    }
-
-    public Long getId() {
-         return id; 
-    }
-    
-    public String getName() { 
-        return name; 
-    }
-
-    public RoomType getType() { 
-        return roomType; 
-    }
-}
+public record RoomDTO(
+    Long id,
+    String name,
+    RoomType type
+) {}
