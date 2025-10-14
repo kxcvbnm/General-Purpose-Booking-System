19f30bfc8413e1b0908d0c7b472a9fd75b2118e5
kxcvbnm <khunanont.ti@gmail.com>
2025-10-14 20:24:58 +0700

    add PreAuthorize

--- backend/src/main/java/com/bookingsystem/booking/room/api/controllers/RoomController.java
+++ backend/src/main/java/com/bookingsystem/booking/room/api/controllers/RoomController.java
@@ -4,6 +4,7 @@ import java.net.URI;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -30,6 +31,7 @@ public class RoomController {
     }
 
     @PostMapping
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<RoomDTO> createRoom(@Valid @RequestBody RoomCreateRequest req) {
         RoomDTO body = roomService.createRoom(req);
 
@@ -43,16 +45,19 @@ public class RoomController {
     }
 
     @GetMapping
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<List<RoomDTO>> getAllRooms() {
         return ResponseEntity.ok(roomService.getAllRooms());
     }
 
     @GetMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<RoomDTO> getRoomById(@PathVariable Long id) {
         return ResponseEntity.ok(roomService.getRoomById(id));
     }
 
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<Void> deleteRoom(@PathVariable Long id) {
         roomService.deleteRoom(id);
         return ResponseEntity.noContent().build(); // 204 No Content
