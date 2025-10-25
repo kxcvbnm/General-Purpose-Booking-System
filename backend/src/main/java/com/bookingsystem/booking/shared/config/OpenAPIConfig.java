56b6e23566bdf345a76587392a2d33a31a7bd994
kxcvbnm <khunanont.ti@gmail.com>
2025-10-25 11:50:13 +0700

    add openapi config for swaggerui

---
+++ backend/src/main/java/com/bookingsystem/booking/shared/config/OpenAPIConfig.java
@@ -0,0 +1,45 @@
+package com.bookingsystem.booking.shared.config;
+
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+    info = @Info(
+        contact = @Contact(
+            name = "Khunanont Titayanunt",
+            email = "khunanont.ti@gmail.com"
+        ),
+        description = "Booking System API Documentation",
+        title = "Booking System API - Khunanont Titayanunt",
+        version = "1.0.0"
+    ),
+    servers = {
+        @Server(
+            description = "Local Server",
+            url = "http://localhost:8082"
+        )
+    },
+    security = {
+        @SecurityRequirement(
+            name = "Bearer Authentication"
+        )
+    }
+)
+@SecurityScheme(
+    name = "Bearer Authentication",
+    description = "JWT Authorization header using the Bearer scheme.",
+    type = SecuritySchemeType.HTTP,
+    bearerFormat = "JWT",
+    scheme = "bearer",
+    in = SecuritySchemeIn.HEADER
+)
+public class OpenAPIConfig {
+    
+}
